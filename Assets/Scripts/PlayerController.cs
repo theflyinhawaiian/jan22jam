b2573b9928e5f395f678d7145e8df8fdc96bf462
Peter Mullins <jc310xc@gmail.com>
2022-01-28 18:46:46 -0500

    add player, background, and basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    protected Rigidbody2D body;
+
+    private Vector2 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.isKinematic = true;
+    }
+
+    void Update()
+    {
+        move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        body.position = body.position + move;
+    }
+}
